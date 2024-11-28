99ae5708a06046f9b5c0b035c1a67d8a4dbab44c
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-11-28 09:36:54 +0000

    add data averaging across 8 channels

--- continuous-read/main/continuous_read_main.c
+++ continuous-read/main/continuous_read_main.c
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
+//-----------------------------------------------------------------------------
 #include <string.h>
 #include <stdio.h>
 #include "sdkconfig.h"
@@ -12,35 +12,34 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "esp_adc/adc_continuous.h"
-
+//-----------------------------------------------------------------------------
 #define EXAMPLE_ADC_UNIT                    ADC_UNIT_1
 #define _EXAMPLE_ADC_UNIT_STR(unit)         #unit
 #define EXAMPLE_ADC_UNIT_STR(unit)          _EXAMPLE_ADC_UNIT_STR(unit)
 #define EXAMPLE_ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
 #define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_0
 #define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
-
-#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
 #define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
 #define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
 #define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
-#else
-#define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
-#define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
-#define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type2.data)
-#endif
-
 #define EXAMPLE_READ_LEN                    256
+//-----------------------------------------------------------------------------
+static adc_channel_t channel[8] = {
+    ADC_CHANNEL_0,
+    ADC_CHANNEL_1,
+    ADC_CHANNEL_2,
+    ADC_CHANNEL_3,
+    ADC_CHANNEL_4,
+    ADC_CHANNEL_5,
+    ADC_CHANNEL_6,
+    ADC_CHANNEL_7
+    };
 
-#if CONFIG_IDF_TARGET_ESP32
-static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
-#else
-static adc_channel_t channel[2] = {ADC_CHANNEL_2, ADC_CHANNEL_3};
-#endif
-
+uint64_t data_average[8] = {0,0,0,0,0,0,0,0};
+//-----------------------------------------------------------------------------
 static TaskHandle_t s_task_handle;
 static const char *TAG = "EXAMPLE";
-
+//-----------------------------------------------------------------------------
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
 {
     BaseType_t mustYield = pdFALSE;
@@ -49,7 +48,7 @@ static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_c
 
     return (mustYield == pdTRUE);
 }
-
+//-----------------------------------------------------------------------------
 static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle)
 {
     adc_continuous_handle_t handle = NULL;
@@ -61,13 +60,16 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
 
     adc_continuous_config_t dig_cfg = {
+        .pattern_num = channel_num,
         .sample_freq_hz = 20 * 1000,
         .conv_mode = EXAMPLE_ADC_CONV_MODE,
         .format = EXAMPLE_ADC_OUTPUT_TYPE,
     };
 
     adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
-    dig_cfg.pattern_num = channel_num;
+    
+    // dig_cfg.pattern_num = channel_num;
+    
     for (int i = 0; i < channel_num; i++) {
         adc_pattern[i].atten = EXAMPLE_ADC_ATTEN;
         adc_pattern[i].channel = channel[i] & 0x7;
@@ -78,12 +80,14 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
         ESP_LOGI(TAG, "adc_pattern[%d].channel is :%"PRIx8, i, adc_pattern[i].channel);
         ESP_LOGI(TAG, "adc_pattern[%d].unit is :%"PRIx8, i, adc_pattern[i].unit);
     }
+    
     dig_cfg.adc_pattern = adc_pattern;
+    
     ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
 
     *out_handle = handle;
 }
-
+//-----------------------------------------------------------------------------
 void app_main(void)
 {
     esp_err_t ret;
@@ -99,9 +103,10 @@ void app_main(void)
     adc_continuous_evt_cbs_t cbs = {
         .on_conv_done = s_conv_done_cb,
     };
+
     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
     ESP_ERROR_CHECK(adc_continuous_start(handle));
-
+    //-------------------------------------------------------------------------
     while (1) {
 
         /**
@@ -117,13 +122,16 @@ void app_main(void)
         char unit[] = EXAMPLE_ADC_UNIT_STR(EXAMPLE_ADC_UNIT);
 
         while (1) {
+
             ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+
             if (ret == ESP_OK) {
                 ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
                 for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
                     adc_digi_output_data_t *p = (adc_digi_output_data_t*)&result[i];
                     uint32_t chan_num = EXAMPLE_ADC_GET_CHANNEL(p);
                     uint32_t data = EXAMPLE_ADC_GET_DATA(p);
+                    data_average[chan_num] += EXAMPLE_ADC_GET_DATA(p);
                     /* Check the channel number validation, the data is invalid if the channel num exceed the maximum channel */
                     if (chan_num < SOC_ADC_CHANNEL_NUM(EXAMPLE_ADC_UNIT)) {
                         ESP_LOGI(TAG, "Unit: %s, Channel: %"PRIu32", Value: %"PRIx32, unit, chan_num, data);
@@ -131,6 +139,11 @@ void app_main(void)
                         ESP_LOGW(TAG, "Invalid data [%s_%"PRIu32"_%"PRIx32"]", unit, chan_num, data);
                     }
                 }
+
+                for (int i = 0; i < 8; i++) {
+                    data_average[i] /= (EXAMPLE_READ_LEN / 8); // Need to send this value out via serial / UART for plotting.
+                }
+                
                 /**
                  * Because printing is slow, so every time you call `ulTaskNotifyTake`, it will immediately return.
                  * To avoid a task watchdog timeout, add a delay here. When you replace the way you process the data,
@@ -143,7 +156,7 @@ void app_main(void)
             }
         }
     }
-
+    //-------------------------------------------------------------------------
     ESP_ERROR_CHECK(adc_continuous_stop(handle));
     ESP_ERROR_CHECK(adc_continuous_deinit(handle));
 }
